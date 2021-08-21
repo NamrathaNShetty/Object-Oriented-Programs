0c651067a0cbf7ee8992de2d0ef23d6385383cfa
NamrathaNShetty <namratanshetty@gmail.com>
2021-08-21 09:40:28 +0530

    Added: Deck of Cards

---
+++ src/com/bridgelabz/Card.java
@@ -0,0 +1,11 @@
+package com.bridgelabz;
+
+public class Card {
+    String suit;
+    String rank;
+    public Card(String suit,String rank){
+        this.suit=suit;
+        this.rank=rank;
+    }
+}
+
